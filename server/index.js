094a63505a7c70bf135f8494824487cd103a53e0
xoxo30 <jounis3668@gmail.com>
2020-07-07 20:12:23 +0900

    step_01

--- server/index.js
+++ server/index.js
@@ -1,95 +1,65 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const port = process.env.PORT || 7000;
-const bodyParser = require('body-parser');
-const cors = require('cors');
-const cookieparser = require('cookie-parser');
-const config = require('./config/key');
-const { auth } = require('./middleware/auth');
-const { User } = require('./models/Users');
+const path = require("path");
+const cors = require('cors')
 
-//application/x-www.form-urlencoded
-app.use(bodyParser.urlencoded({ extended: true }));
-// cors access
+const bodyParser = require("body-parser");
+const cookieParser = require("cookie-parser");
+
+const config = require("./config/key");
+
+// const mongoose = require("mongoose");
+// mongoose
+//   .connect(config.mongoURI, { useNewUrlParser: true })
+//   .then(() => console.log("DB connected"))
+//   .catch(err => console.error(err));
+
+const mongoose = require("mongoose");
+const connect = mongoose.connect(config.mongoURI,
+  {
+    useNewUrlParser: true, useUnifiedTopology: true,
+    useCreateIndex: true, useFindAndModify: false
+  })
+  .then(() => console.log('MongoDB Connected...'))
+  .catch(err => console.log(err));
+
 app.use(
-	cors({
-		origin: 'http://localhost:3000', // 허락하고자 하는 요청 주소
-		credentials: true, // true로 하면 설정한 내용을 response 헤더에 추가
-	}),
+    cors({
+        origin: 'http://localhost:3000',
+        credentials: true,
+    }),
 );
-//application/json
+
+//to not get any deprecation warning or error
+//support parsing of application/x-www-form-urlencoded post data
+app.use(bodyParser.urlencoded({ extended: true }));
+//to get json data
+// support parsing of application/json type post data
 app.use(bodyParser.json());
-app.use(cookieparser());
-const mongoose = require('mongoose');
-mongoose
-	.connect(config.mongoURI, {
-		userNewUrlParser: true,
-		useUnifiedTopology: true,
-		useCreateIndex: true,
-		useFIndAndModify: true,
-	})
-	.then(() => console.log('mongoDB connected'))
-	.catch(err => console.log('err'));
+app.use(cookieParser());
 
-app.get('/', (req, res) => res.send('Hello World!'));
-app.get('/api/testUrl', (req, res) => res.send('procy test'));
+app.use('/api/users', require('./routes/users'));
 
-app.post('/api/users/register', (req, res) => {
-	//bodyParser 통해 파싱
-	//회원정보를 받아 데이터 베이스에 인젝트
-	const user = new User(req.body);
-	user.save((err, userInfo) => {
-		if (err) return res.json({ success: false, err });
-		return res.status(200).json({
-			success: true,
-		});
-	});
+
+//use this to show the image you have in node js server to client (react js)
+//https://stackoverflow.com/questions/48914987/send-image-path-from-node-js-express-server-to-react-client
+app.use('/uploads', express.static('uploads'));
+
+// Serve static assets if in production
+if (process.env.NODE_ENV === "production") {
+
+  // Set static folder   
+  // All the javascript and css files will be read and served from this folder
+  app.use(express.static("client/build"));
+
+  // index.html for all page routes    html or routing and naviagtion
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "../client", "build", "index.html"));
+  });
+}
+
+const port = process.env.PORT || 7000
+
+app.listen(port, () => {
+  console.log(`Server Listening on ${port}`)
 });
-
-app.post('/api/users/login', (req, res) => {
-	//요청된 이메일 db서 조회
-	User.findOne({ email: req.body.email }, (err, userInfo) => {
-		// emailInfo 없을
-		if (!userInfo) {
-			return res.json({
-				loginSuccess: false,
-				message: 'email match failed',
-			});
-		}
-		//요청된 이메일 있을시 비밀번호 매치 확인
-		userInfo.comparePassword(req.body.password, (err, isMatch) => {
-			if (!isMatch) return res.json({ loginSuccess: false, message: '비밀번호 매치 실패' });
-			//비밀번호 access시 토큰 생성
-			userInfo.generateToken((err, user) => {
-				if (err) return res.status(400).send(err);
-				//토근 저장
-				res.cookie('x_auth', user.token)
-					.status(200)
-					.json({ loginSuccess: true, userId: user._id });
-			});
-		});
-	});
-});
-
-app.get('/api/users/auth', auth, (req, res) => {
-	res.status(200).json({
-		_id: req.user._id,
-		isAdmin: req.user.role === 0 ? false : true,
-		isAuth: true,
-		email: req.user.email,
-		name: req.user.name,
-		lastname: req.user.lastname,
-		role: req.user.role,
-		image: req.user.image,
-	});
-});
-
-app.get('/api/users/logout', auth, (req, res) => {
-	User.findOneAndUpdate({ _id: req.user._id }, { token: '' }, (err, user) => {
-		if (err) return res.json({ success: false, err });
-		return res.status(200).send({
-			success: true,
-		});
-	});
-});
-app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`));
--- server/middleware/auth.js
+++ server/middleware/auth.js
@@ -1,17 +1,20 @@
-const { User } = require('../models/Users');
+const { User } = require('../models/User');
+
 let auth = (req, res, next) => {
-	//인증처리
-	//client cookie import
-	let token = req.cookies.x_auth;
-	//token 복호화 후 유저 검색
-	User.findByToken(token, (err, user) => {
-		if (err) throw err;
-		if (!user) return res.json({ isAuth: false, error: true });
-		req.token = token;
-		req.user = user;
-		next();
-	});
-	//유저가 있으면 인증
-	//유저가 없으면 미인증
+  let token = req.cookies.w_auth;
+
+  User.findByToken(token, (err, user) => {
+    if (err) throw err;
+    if (!user)
+      return res.json({
+        isAuth: false,
+        error: true
+      });
+
+    req.token = token;
+    req.user = user;
+    next();
+  });
 };
+
 module.exports = { auth };
